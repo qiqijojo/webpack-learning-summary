99bd1b84c69a496d041fe39298ff79a796e07bd4
jojo <774948439@qq.com>
2020-03-12 16:13:14 +0800

    了解使用postcss-loader工具的autoprefixer插件来自动给css属性添加私有前缀

---
+++ webpack核心基础/10-postcss-loader/postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    plugins: {
+        "autoprefixer": {
+            "overrideBrowserslist": [ // 配置要兼容哪些浏览器
+                "ie >= 8", // 兼容ie7以上浏览器版本, -ms
+                "Firefox >= 3.5", // 火狐, -moz
+                "chrome >= 35", // 谷歌， -webkit https://www.caniuse.com/#search=transform 这个网址可查看兼容哪些版本，需要添加私有前缀
+                "opera >= 11.5" // 欧朋, -o
+                // "chrome >= 36" // 兼容36以上版本不需要添加前缀
+            ]
+        }
+    }
+}
