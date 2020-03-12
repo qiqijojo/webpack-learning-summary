e2aa026410c682e741147b14af8c187291a25424
jojo <774948439@qq.com>
2020-03-12 11:15:10 +0800

    两种方式混合使用

---
+++ webpack核心基础/06-ES6-Module/module3.js
@@ -0,0 +1,14 @@
+let a = 'aaa';
+let b = 'bbb';
+let c = 'ccc';
+export {
+    a, b, c
+}
+
+class Persoon{
+    constructor() {
+        this.name = 'lisi';
+        this.age = 12;
+    }
+}
+export default Persoon;
