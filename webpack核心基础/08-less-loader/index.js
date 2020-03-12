782dba235caa60d4aa4878ee65d5c524c4230569
jojo <774948439@qq.com>
2020-03-12 11:57:39 +0800

    使用less-loader编译less文件

---
+++ webpack核心基础/08-less-loader/index.js
@@ -0,0 +1,9 @@
+ import image from './image.png';
+//  import './index.css';
+import './index.less'; // 编译less文件
+console.log(image);
+
+let oImg = document.createElement('img');
+oImg.src = image;
+oImg.setAttribute('class', 'size');
+document.body.appendChild(oImg);
