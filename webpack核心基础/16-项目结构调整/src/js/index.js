00d54f74176d2e84c9dff60be005ab12cb4f9323
jojo <774948439@qq.com>
2020-03-13 09:03:02 +0800

    项目目录调整

---
+++ webpack核心基础/16-项目结构调整/src/js/index.js
@@ -0,0 +1,8 @@
+import image from '../images/image.png';
+// import imageCopy from '../images/image-copy.png';
+import '../css/index.css';
+
+let oImg = document.createElement('img');
+oImg.src = imageCopy;
+oImg.setAttribute('class', 'size');
+document.body.appendChild(oImg);
