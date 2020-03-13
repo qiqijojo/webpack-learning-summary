0d6be8d6204f009585398f05158a0a41d4baeea4
jojo <774948439@qq.com>
2020-03-13 16:02:30 +0800

    webpack-cors解决跨域问题 方式（2）

--- webpack核心基础/21-webpack-cors跨域问题/webpack.config.js
+++ webpack核心基础/21-webpack-cors跨域问题/webpack.config.js
@@ -11,23 +11,37 @@ module.exports = {
         contentBase: './bundle', // 配置哪个目录运行在服务器环境下
         open: true,
         port: 9000,
-        proxy: {
-            /**
-             * 以下配置的含义：
-             * 当我们在代码中发送请求到/user的时候，devServer就会自动将我们请求的地址替换为：
-             * http://127.0.0.1:3000/user
-             */
-            '/user': {
-                target: 'http://127.0.0.1:3000',
-                changeOrigin: true, // 域名跨域
-                secure: false, // https跨域 
-            },
-            '/login': {
-                target: 'http://127.0.0.1:3000',
-                changeOrigin: true, // 域名跨域
-                secure: false, // https跨域 
+        // proxy: {
+        //     /**
+        //      * 以下配置的含义：
+        //      * 当我们在代码中发送请求到/user的时候，devServer就会自动将我们请求的地址替换为：
+        //      * http://127.0.0.1:3000/user
+        //      */
+        //     '/user': {
+        //         target: 'http://127.0.0.1:3000',
+        //         changeOrigin: true, // 域名跨域
+        //         secure: false, // https跨域 
+        //     },
+        //     '/login': {
+        //         target: 'http://127.0.0.1:3000',
+        //         changeOrigin: true, // 域名跨域
+        //         secure: false, // https跨域 
+        //     }
+        // }
+        proxy: [{
+            context: ['/user', '/login'],
+            target: 'http://127.0.0.1:3000',
+            changeOrigin: true, // 域名跨域
+            secure: false, // https跨域
+            pathRewrite: { // 路径重写，将路径中的api替换为空（我理解的是将/api添加在/user之前，来符合服务端的接口）
+                '': '/api'
             }
-        }
+        }]
+        /**
+         * 注意点：
+         * devServer只能解决开发阶段的跨域问题，并不能解决项目上线之后的跨域问题
+         * 原因非常简单，因为项目上线之后是将打包好的文件上传到服务器，而打包好的文件中并没有devServer
+         */
     },
     /*
     watch: true,
