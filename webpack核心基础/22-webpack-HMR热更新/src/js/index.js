e29d98299d1ba72866ed26d068a6fc30d6647b2b
jojo <774948439@qq.com>
2020-03-13 18:14:21 +0800

    webpack-hmr热更新处理js

--- webpack核心基础/22-webpack-HMR热更新/src/js/index.js
+++ webpack核心基础/22-webpack-HMR热更新/src/js/index.js
@@ -1,4 +1,5 @@
 import '../css/index.css';
+import { addSpan } from './test.js';
 
 
 let btn = document.createElement("button");
@@ -12,3 +13,15 @@ btn.onclick = function () {
     index++;
     document.body.appendChild(p);
 };
+
+addSpan();
+
+// 判断当前有没有开启热更新
+if (module.hot) {
+    // 告诉热更新需要监听哪一个模块的变化
+    module.hot.accept('./test.js', () => {
+        let o = document.querySelector('span');
+        document.body.removeChild(o);
+        addSpan();
+    })
+}
---
+++ webpack核心基础/22-webpack-HMR热更新/src/js/test.js
@@ -0,0 +1,9 @@
+function addSpan() {
+    let oSpan = document.createElement('span');
+    oSpan.innerHTML = 'www.jojo.com';
+    document.body.appendChild(oSpan);
+}
+export {
+    addSpan
+}
+
