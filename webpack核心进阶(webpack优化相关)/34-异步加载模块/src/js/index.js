1d23280813f2b900b2c9c78c0c5d28bdc2f96942
jojo <774948439@qq.com>
2020-03-17 11:41:48 +0800

    异步加载写法1

--- webpack核心进阶(webpack优化相关)/34-异步加载模块/src/js/index.js
+++ webpack核心进阶(webpack优化相关)/34-异步加载模块/src/js/index.js
@@ -1,19 +1,32 @@
 /** ***************************同步加载***************************** */
 
 
-import $ from 'jquery';
+// import $ from 'jquery';
+
+// const oBtn = document.querySelector('button');
+// oBtn.onclick = () => {
+//     let $div = getComponent();
+//     console.log($div) // 是个jquery节点，不是原生节点，$div[0]为原生节点
+//     document.body.appendChild($div[0]);
+// }
+// function getComponent() {
+//     let $div = $('<div>我是div</div>');
+//     return $div;
+// }
+
+
+
+/** ***************************异步加载(用到的时候才加载，同时将js添加进header中)***************************** */
 
 const oBtn = document.querySelector('button');
 oBtn.onclick = () => {
-    let $div = getComponent();
-    console.log($div) // 是个jquery节点，不是原生节点，$div[0]为原生节点
-    document.body.appendChild($div[0]);
+    getComponent().then($div => {
+        document.body.appendChild($div[0]); 
+    })
 }
 function getComponent() {
-    let $div = $('<div>我是div</div>');
-    return $div;
+    return import('jquery').then(({ default: $ }) => {
+        const $div = $('<div>我是div</div>');
+        return $div;
+    })
 }
-
-
-
-/** ***************************同步加载***************************** */
