f040806ed2cd87ef1b1055e939a4a833ce693443
jojo <774948439@qq.com>
2020-03-18 21:55:18 +0800

    动态链接（上）

---
+++ webpack核心进阶(webpack优化相关)/45-webpack-动态链接库（下）/webpack.config.prod.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const Merge = require('webpack-merge');
+const CommonConfig = require('./webpack.config.common.js');
+const PurifyCSSPlugin = require("purifycss-webpack");
+const glob = require("glob-all");
+
+const ProdConfig = {
+    /**
+     * 配置sourcemap：
+     * 开发阶段：'cheap-module-eval-source-map'
+     * 生产阶段：'cheap-module-source-map'
+     */
+    devtool: 'cheap-module-source-map',
+    /**
+     * mode：指定打包的模式，模式有两种
+     * 一种是开发模式(development): 不会对打包的JS代码进行压缩
+     * 还有一种就是上线(生产)模式(production): 会对打包的JS代码进行压缩
+     */
+    mode: 'production', // "production" | "development"
+    /**
+     * optimization: 配置webpack的优化项
+     */
+    optimization: {
+        minimizer: [
+            new TerserJSPlugin({}),
+            new OptimizeCSSAssetsPlugin({})
+        ],
+    },
+    /**
+     * plugin：告诉webpack需要新增一些什么样的功能
+     */
+    plugins: [
+        new PurifyCSSPlugin({
+            // 告诉PurifyCSSPlugin需要过滤哪些文件
+            paths: glob.sync([
+                path.join(__dirname, 'src/*.html'),
+                path.join(__dirname, 'src/js/*.js')
+            ])
+        })
+    ]
+};
+module.exports = Merge(CommonConfig, ProdConfig);
