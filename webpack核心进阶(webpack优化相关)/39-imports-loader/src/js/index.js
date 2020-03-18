660e95864e1924f728a0cd4378cacc600ef6f386
jojo <774948439@qq.com>
2020-03-18 14:23:18 +0800

    this指向带来的问题

--- webpack核心进阶(webpack优化相关)/39-imports-loader/src/js/index.js
+++ webpack核心进阶(webpack优化相关)/39-imports-loader/src/js/index.js
@@ -4,4 +4,7 @@
 // $('div').css({ width: '200px', height: '200px', background: '#aaa' });
 // addBorder()
 
-console.log(this)
+
+// 无需修改this指向，直接用window即可，就解决了添加了this指向之后，会报import必须放在顶部的错。
+// console.log(this)
+console.log(window);
