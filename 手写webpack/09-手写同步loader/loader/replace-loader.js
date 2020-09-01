176ab1e86955b2b338e1b0c099ee224841a35799
jojo <774948439@qq.com>
2020-09-01 16:22:34 +0800

    同步loader参数处理

--- 手写webpack/09-手写同步loader/loader/replace-loader.js
+++ 手写webpack/09-手写同步loader/loader/replace-loader.js
@@ -1,4 +1,4 @@
-
+const loaderUtils = require('loader-utils');
 /**
  * 
  * @param {*} content 是每个文件的内容
@@ -7,8 +7,9 @@
  */
 module.exports = function (content, map, meta) {  // map, meta这两个打印出来是undefined
     // console.log('打印', content, map, meta);
-    console.log(this.query)
-    const name = this.query.name;
-    content = content.replace(/hello/g, name)
+    // console.log(this.query)
+    // const name = this.query.name;
+    const options = loaderUtils.getOptions(this);
+    content = content.replace(/hello/g, options.name)
     return content;
 }
