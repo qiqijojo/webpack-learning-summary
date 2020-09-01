6db4c2b42c54b935548b4aff279bde1e10d896db
jojo <774948439@qq.com>
2020-09-01 17:46:49 +0800

    解释validateOptions第三个参数的作用

--- 手写webpack/09-手写同步loader/loader/replace-loader.js
+++ 手写webpack/09-手写同步loader/loader/replace-loader.js
@@ -20,7 +20,10 @@ module.exports = function (content, map, meta) {  // map, meta这两个打印出
         },
         additionalProperties: false
     };
-    validateOptions(schema, options, 'replace-loader') // 这个loader的名称无论写什么都会正常运行，没搞懂写这个有什么用。。。
+    validateOptions(schema, options, 'replace-loader');
+    // 第三个参数的这个loader的名称无论写什么都会正常运行，没搞懂写这个有什么用。。。
+    // 其实我觉得是等到loader发布到npm上就是npm包的名称，到时候就能体现出作用了，主要告诉用户报错的时候是哪个loader在报错.
+    // 目前报错的时候提示的都是loader的路径，因为我们在webpack配置的就是loader的路径。ok
     content = content.replace(/hello/g, options.name)
     return content;
 }
